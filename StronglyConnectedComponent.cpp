213554078475bf78f0ef6f003b8e03c12d3c16dc
Rs1124 <604347974@qq.com>
2022-01-18 10:34:51 +0800

    完善了邻接矩阵下的Trajan算法找寻有向图的强连通分量

--- StronglyConnectedComponent.cpp
+++ StronglyConnectedComponent.cpp
@@ -14,11 +14,11 @@ void adjacencyList();
 bool dfsAdjacencyList(Array arrayV[],int num,bool arrayFlag[],bool arrayIsStack[],stack<int> &s,int arrayColor[]); //(有向图&&无向图)邻接表的DFS
 void displayAdjacencyMatrix(int **arrayV,int length);
 void adjacencyMatrix();
-bool dfsAdjacencyMatrix(int **arrayV,int r,int c,bool arrayFlag[],int num); //(有向图&&无向图)邻接矩阵的DFS
+bool dfsAdjacencyMatrix(int **arrayV,int r,bool arrayFlag[],int num,bool arrayIsStack[],stack<int> &s,int arrayColor[]); //(有向图&&无向图)邻接矩阵的DFS
 
 int main() {
-//    adjacencyList();
-    adjacencyMatrix();
+//    adjacencyList(); //邻接表下的Trajan算法找寻有向图的强连通分量
+    adjacencyMatrix(); //邻接矩阵下的Trajan算法找寻有向图的强连通分量
 }
 
 void adjacencyList(){
@@ -105,16 +105,21 @@ void adjacencyMatrix(){
     int **arrayV;
     int flag=1;
     bool *arrayFlag;
+    bool *arrayIsStack;
+    stack<int> stackV;
+    int *arrayColor;
     cout<<"输入顶点个数:";
     cin>>numV;
     arrayFlag = new bool[numV]{};
     arrayV = new int*[numV];
+    arrayIsStack = new bool[numV]{};
+    arrayColor = new int[numV]{};
     for(int i=0;i<numV;i++){
         arrayV[i] = new int[numV]{};
     }
     while(flag==1){
         int n,m;
-        cout<<"请输入要连接的两个顶点的编号:";
+        cout<<"请输入要连接的两个顶点的编号(输入两个相同的编号就会结束输入):";
         cin>>n>>m;
         if(n==m) flag=0;
         else{
@@ -122,25 +127,43 @@ void adjacencyMatrix(){
         }
     }
     displayAdjacencyMatrix(arrayV,numV);
-    cout<<"邻接矩阵生成完毕！"<<endl;
+    cout<<"邻接矩阵生成完毕！染色情况如下:"<<endl;
     for(int i=0;i<numV;i++){
         if(arrayFlag[i]==false){
-            cout<<i<<" ";
             arrayFlag[i]=true;
-        }
-        for(int j=0;j<numV;j++){
-            if(i!=j) dfsAdjacencyMatrix(arrayV,i,j,arrayFlag,numV);
+            arrayColor[i]=i;
+            stackV.push(i);
+            arrayIsStack[i]=true;
+            dfsAdjacencyMatrix(arrayV,i,arrayFlag,numV,arrayIsStack,stackV,arrayColor);
         }
     }
+    for(int i=0;i<numV;i++) cout<<arrayColor[i];
 }
 
-bool dfsAdjacencyMatrix(int **arrayV,int r,int c,bool arrayFlag[],int num){
-    if(arrayV[r][c]==0||arrayFlag[c]==true) return false;
-    if(arrayV[r][c]==1){
-        arrayFlag[c]=true;
-        cout<<c<<" ";
-        for(int i=0;i<num;i++){
-            dfsAdjacencyMatrix(arrayV,c,i,arrayFlag,num);
+bool dfsAdjacencyMatrix(int **arrayV,int r,bool arrayFlag[],int num,bool arrayIsStack[],stack<int> &s,int arrayColor[]){
+    for(int c=0;c<num;c++){
+        if(arrayV[r][c]==1&&arrayFlag[c]==false){ //case1:有路可走
+            arrayFlag[c]=true;
+            arrayColor[c]=c;
+            s.push(c);
+            arrayIsStack[c]=true;
+            dfsAdjacencyMatrix(arrayV,c,arrayFlag,num,arrayIsStack,s,arrayColor); //c=2
+        }
+        if(arrayV[r][c]==1&&arrayFlag[c]==true){
+            if(arrayIsStack[c]==true){ //case2:顶点c已经遍历过了，并且c在栈中
+                if(arrayColor[c]<arrayColor[r]) arrayColor[r]=arrayColor[c]; //染色
+            }
+            //case3:顶点c已经遍历过了，并且c不在栈中
+        }
+    }
+    if(r==arrayColor[r]){
+        while(s.top()!=r){
+            arrayIsStack[s.top()]=false;
+            s.pop();
+        }
+        if(s.top()==r) {
+            arrayIsStack[s.top()]=false;
+            s.pop();
         }
     }
 }
