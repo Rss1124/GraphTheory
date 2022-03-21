4e54092294162373d1f8948729e77e3fae65fa6f
Rs1124 <604347974@qq.com>
2022-03-21 11:03:00 +0800

    无向图的割边,割点: a.如何染色？ b.出现割边的条件 c.出现割点的条件

--- Bridges.cpp
+++ Bridges.cpp
@@ -11,10 +11,11 @@ typedef struct Array{
 
 void displayAdjacencyList(Array arrayV[],int length);
 void adjacencyList();
-bool dfsLabel(Array arrayV[],int num,int &value,int arrayLow[],int arrayID[],int pre,bool inCycle[]); //(有向图&&无向图)邻接表的DFS
+bool dfsLabel(Array arrayV[],int num,int &value,int arrayLow[],int arrayID[],int pre,bool inCycle[]); //邻接表存储的无向图的DFS
 void findBridge(Array arrayV[],int length,int arrayLow[],int arrayID[],bool inCycle[],bool isArt[]);
 
 int main() {
+    /** 桥是无向图 **/
     adjacencyList();
 }
 
@@ -37,7 +38,7 @@ void adjacencyList(){
         }
     }
     displayAdjacencyList(arrayV,numV);
-    cout<<"邻接表生成完毕！染色情况如下:"<<endl;
+    cout<<"邻接表生成完毕！"<<endl;
     bool *inCycle;
     inCycle=new bool[numV]{};
     int *arrayLow;
@@ -50,10 +51,13 @@ void adjacencyList(){
     for(int i=0;i<numV;i++){
         dfsLabel(arrayV,i,value,arrayLow,arrayID,0,inCycle);
     }
+    cout<<"顶点索引: ";
     for(int i=0;i<numV;i++) cout<<arrayID[i]<<" ";
     cout<<endl;
+    cout<<"染色情况: ";
     for(int i=0;i<numV;i++) cout<<arrayLow[i]<<" ";
     cout<<endl;
+    cout<<"是否在环: ";
     for(int i=0;i<numV;i++) cout<<inCycle[i]<<" ";
     cout<<endl;
     cout<<"割边情况如下:"<<endl;
@@ -64,8 +68,9 @@ void adjacencyList(){
     }
 }
 
-bool dfsLabel(Array arrayV[],int num,int &value,int arrayLow[],int arrayID[],int pre,bool inCycle[]){ //num=0 pre=2
-    if(arrayV[num].flag==true){ //case1:访问到“被访问的顶点”且ID[num]比ID[pre]小
+/** 将顶点进行染色 **/
+bool dfsLabel(Array arrayV[],int num,int &value,int arrayLow[],int arrayID[],int pre,bool inCycle[]){
+    if(arrayV[num].flag==true){ //case1:访问到“被访问的顶点”且arrayLow[被访问的顶点]比arrayLow[前一个顶点]小
         if(arrayLow[num]<arrayLow[pre]){
             arrayLow[pre]=arrayLow[num];
             inCycle[num]=true;
@@ -80,7 +85,7 @@ bool dfsLabel(Array arrayV[],int num,int &value,int arrayLow[],int arrayID[],int
         for(int i=0;i<arrayV[num].length;i++){
             if(arrayV[num].arrayE[i]!=pre){ //防止无向图出现以下这种情况 3->2 2->3
                 dfsLabel(arrayV,arrayV[num].arrayE[i],value,arrayLow,arrayID,num,inCycle);
-                if(arrayLow[arrayV[num].arrayE[i]]<arrayLow[num]){
+                if(arrayLow[arrayV[num].arrayE[i]]<arrayLow[num]){ //case2:回溯过程中发现arrayLow[下一个顶点]比arrayLow[当前顶点]
                     arrayLow[num]=arrayLow[arrayV[num].arrayE[i]];
                     inCycle[num]=true;
                     inCycle[arrayV[num].arrayE[i]]=true;
@@ -104,13 +109,13 @@ void findBridge(Array arrayV[],int length,int arrayLow[],int arrayID[],bool inCy
     for(int i=0;i<length;i++){
         int count=0;
         for(int j=0;j<arrayV[i].length;j++){
-            if(arrayID[i]<arrayLow[arrayV[i].arrayE[j]]){
+            if(arrayID[i]<arrayLow[arrayV[i].arrayE[j]]){ //出现割边的case:arrayID[当前顶点]<arrayLow[下一个顶点]
                 cout<<i<<"-"<<arrayV[i].arrayE[j]<<endl;
             }
             if(arrayLow[i]!=arrayLow[arrayV[i].arrayE[j]]){
                 count++;
-                if(inCycle[i]==1) isArt[i]=1;
-                else if(count>1) isArt[i]=1;
+                if(inCycle[i]==1) isArt[i]=1; //出现割点的case1: 处于环内的桥上的顶点
+                else if(count>1) isArt[i]=1; //出现割点的case2: 没处于环内的桥上的顶点连接了2条及以上的边
             }
         }
     }
