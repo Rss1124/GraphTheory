220180426b021e5e4ff840a056cb18b2aceb4fd9
Rs1124 <604347974@qq.com>
2022-03-23 10:23:12 +0800

    BFS走迷宫:
    S  .  .  #  .  .  .
    .  #  .  .  .  #  .
    .  #  .  .  .  .  .
    .  .  #  #  .  .  .
    #  .  #  E  .  #  .

--- ShortestPathByBFS.cpp
+++ ShortestPathByBFS.cpp
@@ -33,8 +33,12 @@ void adjacencyMatrix(){
 }
 
 void bfsAdjacencyMatrix(int **arrayFlag,string graph[][7],queue<int> qR,queue<int> qC){
-    int dR[4]={0,0,1,-1}; //方向数组
-    int dC[4]={1,-1,0,0}; //用来表示图中结点的运动方向（→，←，↓，↑）
+    /**
+     *  dR,dC为方向数组
+     *  用来表示图中结点的运动方向（→，←，↓，↑）
+     */
+    int dR[4]={0,0,1,-1};
+    int dC[4]={1,-1,0,0};
     while(qR.empty()!=true){
         int r=qR.front();
         int c=qC.front();
@@ -43,7 +47,7 @@ void bfsAdjacencyMatrix(int **arrayFlag,string graph[][7],queue<int> qR,queue<in
         for(int i=0;i<4;i++){ //0<=r<=4 0<=c<=6 r={0,0,1,-1} c={1,-1,0,0}
             int rr=r+dR[i];
             int cc=c+dC[i];
-            if((0<=rr&&rr<=4)&&(0<=cc&&cc<=6)) { //r={0,1} c={1,0}
+            if((0<=rr&&rr<=4)&&(0<=cc&&cc<=6)) {
                 if(graph[rr][cc]=="E"){ //case1:找到终点
                     arrayFlag[rr][cc]=arrayFlag[r][c]+1;
                     cout<<arrayFlag[rr][cc];
